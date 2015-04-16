50b5f5ff39da90f5216c8e45cd878cefa26baa24
James Mead <james@floehopper.org>
2015-04-16 10:43:36 +0100

    Introduce concept of UK students.

--- smart_answer.rb
+++ smart_answer.rb
@@ -1,6 +1,8 @@
 class SmartAnswer
   EU_FULL_TIME = :eu_full_time
   EU_PART_TIME = :eu_part_time
+  UK_FULL_TIME = :uk_full_time
+  UK_PART_TIME = :uk_part_time
 
   FULL_TIME_TUITION_FEES_MAXIMUM = 9000
   PART_TIME_TUITION_FEES_MAXIMUM = 6750
@@ -9,12 +11,22 @@ class SmartAnswer
   attr_accessor :tuition_fees
 
   def outcome
-    text = (student_type == EU_FULL_TIME) ? "EU Full-Time" : "EU Part-Time"
+    text = case student_type
+    when EU_FULL_TIME
+      "EU Full-Time"
+    when EU_PART_TIME
+      "EU Part-Time"
+    when UK_FULL_TIME
+      "UK Full-Time"
+    when UK_PART_TIME
+      "UK Part-Time"
+    end
     "#{text} #{tuition_fees}"
   end
   
   def valid?
-    if student_type == EU_FULL_TIME
+    case student_type
+    when EU_FULL_TIME, UK_FULL_TIME
       tuition_fees <= FULL_TIME_TUITION_FEES_MAXIMUM
     else
       tuition_fees <= PART_TIME_TUITION_FEES_MAXIMUM
--- smart_answer_spec.rb
+++ smart_answer_spec.rb
@@ -55,4 +55,54 @@ RSpec.describe SmartAnswer do
       expect(subject).to_not be_valid
     end
   end
+
+  context 'UK student full-time' do
+    before do
+      subject.student_type = SmartAnswer::UK_FULL_TIME
+      subject.tuition_fees = 8000
+    end
+    
+    it 'should display information about finance for full-time UK students' do
+      expect(subject.outcome).to match('UK Full-Time')
+    end
+
+    it 'should display tuition fees' do
+      expect(subject.outcome).to match('8000')
+    end
+
+    it 'should have valid tuition fees if fees are less than or equal to the maximum' do
+      subject.tuition_fees = SmartAnswer::FULL_TIME_TUITION_FEES_MAXIMUM
+      expect(subject).to be_valid
+    end
+    
+    it 'should not have valid tuition fees if fees are more than the maximum' do
+      subject.tuition_fees = SmartAnswer::FULL_TIME_TUITION_FEES_MAXIMUM + 1
+      expect(subject).to_not be_valid
+    end
+  end
+
+  context 'UK student part-time' do
+    before do
+      subject.student_type = SmartAnswer::UK_PART_TIME
+      subject.tuition_fees = 6000
+    end
+    
+    it 'should display information about finance for part-time UK students' do
+      expect(subject.outcome).to match('UK Part-Time')
+    end
+
+    it 'should display tuition fees' do
+      expect(subject.outcome).to match('6000')
+    end
+
+    it 'should have valid tuition fees if fees are less than or equal to the maximum' do
+      subject.tuition_fees = SmartAnswer::PART_TIME_TUITION_FEES_MAXIMUM
+      expect(subject).to be_valid
+    end
+    
+    it 'should not have valid tuition fees if fees are more than the maximum' do
+      subject.tuition_fees = SmartAnswer::PART_TIME_TUITION_FEES_MAXIMUM + 1
+      expect(subject).to_not be_valid
+    end
+  end
 end
